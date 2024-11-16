e488378014a3b281e5d3915aa39bfc64d07607f0
xiaoyi1212 <www18845340923@163.com>
2024-11-16 13:15:21 +0800

    烧烤的story曲子实装

--- scripts/start.gd
+++ scripts/start.gd
@@ -4,11 +4,24 @@ extends Node
 @onready var animation_subtitle = $Subtitle/AnimationPlayer
 @onready var animation_warn_info = $WarnInfo/AnimationPlayer
 @onready var ok_z  = $Label/AnimationPlayer
-@onready var menu = load("res://main_menu.tscn")
+var menu = null
+
+var config = ConfigFile.new()
 
 var status = true
 
 func _ready():
+	var err = config.load("user://player_data.cfg")
+	if err != OK:
+		menu = load("res://story.tscn")
+		config.set_value("","story","true")
+		config.save("user://player_data.cfg")
+	else:
+		var a = config.get_value("","story","false")
+		if(a == "true"):
+			menu = load("res://main_menu.tscn")
+		else:
+			menu = load("res://story.tscn")
 	animation.connect("animation_finished",_show_subtitle)
 	animation_warn_info.connect("animation_finished",_show_warn)
 	animation_subtitle.connect("animation_finished",_subtitle_done)
